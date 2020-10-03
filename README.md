de20d43a8ff3d5665c5a9908f36d7e0cdef6cc83
ShauryaS168 <70435151+ShauryaS168@users.noreply.github.com>
2020-10-03 17:21:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SupplyMission-C23
-done on vsc code
+# SupplyMission
---
+++ package.js
@@ -0,0 +1,9 @@
+class Package {
+    constructor(x, y, height, angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':3.0,
+          'density':5.0
+      }
+    }
+}
